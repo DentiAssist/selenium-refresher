9ea7c7c89c58e0c17d1914e590723d9575d0bf8e
Dilara Özdemir <42623884+dilaraozdemir@users.noreply.github.com>
2021-03-02 10:34:01 +0300

    Update refresher.py

--- refresher.py
+++ refresher.py
@@ -2,26 +2,25 @@ from selenium import webdriver
 import time
 import sys
 from webdriver_manager.chrome import ChromeDriverManager
+driver = webdriver.Chrome(ChromeDriverManager().install()) # activate chromedriver.exe
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get('https://cp.karabuk.edu.tr/');
-time.sleep(5) # Let the user actually see something!
+while(True):
+    driver.get('https://cp.karabuk.edu.tr/'); # URL you want to get
+    
+    #Username
+    kbu_email = sys.argv[1]
+    
+    #Password
+    kbu_password = sys.argv[2]
 
-#Username
-kbu_email = sys.argv[1]
-#Password
-kbu_password = sys.argv[2]
+    element1 = driver.find_element_by_name("Username")
+    element1.send_keys(kbu_email)
 
-element1 = driver.find_element_by_name("Username")
-element1.send_keys(kbu_email)
+    element2 = driver.find_element_by_name("Password")
+    element2.send_keys(kbu_password)
 
-element2 = driver.find_element_by_name("Password")
-element2.send_keys(kbu_password)
+    login = driver.find_element_by_xpath("//button[@class='btn btn-primary btn-block btn-lg']").click()
 
-login = driver.find_element_by_class_name("btn btn-primary btn-block btn-lg").click()
-logout = driver.find_element_by_class_name('btn text-uppercase btn-block btn-primary').click()
+    time.sleep(60) # time between login and logout (60secs)
 
-# driver.findElement(By.cssSelector("div.column a")).click();
-
-time.sleep(60 * 60) # Let the user actually see something!
-driver.refresh()
+    logout = driver.find_element_by_xpath("//button[@class='btn text-uppercase btn-block  btn-primary'][@type='submit']").click()
